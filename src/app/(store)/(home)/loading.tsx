7e3b5539a4f7bae1d0902760c46cc1e6f7af0fa4
João Manoel <joaomanoeldev@gmail.com>
2024-05-11 23:23:40 -0300

    feat: create home loading

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,13 @@
+import { Skeleton } from '@/components/skeleton'
+
 const HomeLoading = () => {
-  return <p>Carregando...</p>
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[717px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
+    </div>
+  )
 }
 
 export default HomeLoading
