0849ac0c11f34a16f11d4f5508336851ce4a28d9
João Manoel <joaomanoeldev@gmail.com>
2024-05-12 13:40:53 -0300

    feat: add next top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import NextTopLoader from 'nextjs-toploader'
 
 import '@/styles/globals.css'
 import { inter } from '@/styles/fonts'
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className={inter.variable}>
-      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">
+        <NextTopLoader color="#a1a1aa" showSpinner={false} shadow={false} />
+        {children}
+      </body>
     </html>
   )
 }
