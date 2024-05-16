5763c9a9a2e31a280bf12a204def9b7f5498c6db
João Manoel <joaomanoeldev@gmail.com>
2024-05-16 12:05:05 -0300

    feat: create Loading

---
+++ src/app/(store)/search/loading.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const Loading = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold"></span>
+      </p>
+    </div>
+  )
+}
+
+export default Loading
