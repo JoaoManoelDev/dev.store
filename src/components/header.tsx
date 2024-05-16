fd98a0a35fda6a6d69692dcc50ac8882eac00c5e
João Manoel <joaomanoeldev@gmail.com>
2024-05-16 12:03:10 -0300

    feat: add SearchForm component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-import { Icons } from '@/components/icons'
 import { CartWidget } from '@/components/cart-widget'
+import { SearchForm } from '@/components/search-form'
 
 export const Header = () => {
   return (
@@ -12,15 +12,7 @@ export const Header = () => {
           dev.store
         </Link>
 
-        <form className="flex items-center w-[320px] gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-1 ring-zinc-700">
-          <Icons.search className="w-5 h-5 text-zinc-500" />
-
-          <input
-            type="text"
-            placeholder="buscar produtos..."
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
         <CartWidget />
