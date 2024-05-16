15a8705fce92496a0d0d29067bfa39230e9af7e7
João Manoel <joaomanoeldev@gmail.com>
2024-05-16 12:04:33 -0300

    feat: implement query and add real product

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,39 +1,72 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
-const Search = () => {
+import { Product } from '@/data/types/product'
+import { api } from '@/data/api'
+
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+const searchProducts = async (query: string): Promise<Product[]> => {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+const Search = async ({ searchParams }: SearchProps) => {
+  const { q: query } = searchParams
+
+  if (!query) redirect('/')
+
+  const products = await searchProducts(query)
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
-        Resultados para: <span className="font-semibold">moletom</span>
+        Resultados para: <span className="font-semibold">{query}</span>
       </p>
 
       <div className="grid grid-cols-3 gap-6">
-        <Link
-          href={`/product/moletom-never-stop-learning`}
-          className="group relative flex justify-center items-end rounded-lg bg-zinc-900 overflow-hidden"
-        >
-          <Image
-            src="/products/mouse-razer.png"
-            className="group-hover:scale-105 transition-transform duration-500"
-            width={480}
-            height={480}
-            quality={100}
-            alt=""
-          />
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/moletom-never-stop-learning`}
+              className="group relative flex justify-center items-end rounded-lg bg-zinc-900 overflow-hidden"
+            >
+              <Image
+                src={product.image}
+                className="group-hover:scale-105 transition-transform duration-500"
+                width={480}
+                height={480}
+                quality={100}
+                alt=""
+              />
 
-          <div className="absolute bottom-4 right-4 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">MouseRazer</span>
-            <span className="flex justify-center items-center h-full rounded-full bg-violet-500 px-4 font-semibold">
-              {Number(129).toLocaleString('pt-br', {
-                style: 'currency',
-                currency: 'BRL',
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
+              <div className="absolute bottom-4 right-4 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                <span className="text-sm truncate">{product.title}</span>
+                <span className="flex justify-center items-center h-full rounded-full bg-violet-500 px-4 font-semibold">
+                  {product.price.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
