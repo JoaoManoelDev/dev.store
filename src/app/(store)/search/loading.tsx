6726a16042173effaa7890a6d8db70c614fe9525
João Manoel <joao.manoel@dbit.srv.br>
2024-07-27 23:17:55 -0300

    fix: remove searchParams from the loading page

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,19 +1,10 @@
 'use client'
 
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
 
 const Loading = () => {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
-
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[347px]" />
         <Skeleton className="h-[347px]" />
